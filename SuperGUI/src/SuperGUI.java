1c1c339b75d7e4388061b2b7decb6256ac6c9858
Arden Zhang <arden.zhang@gmail.com>
2018-01-14 11:31:32 -0500

    Corners should work

--- SuperGUI/src/SuperGUI.java
+++ SuperGUI/src/SuperGUI.java
@@ -4,7 +4,7 @@ import javax.swing.JFrame;
 
 /**
  * Creates an interactive gui to plan robot actions
- * 
+ *
  * When customizing for specific games:
  *  - Add robot actions in SuperEnum (eg. rotate, place gear)
  *  - Customize those actions in the switch statements in SuperPrinter and SuperPoint
@@ -22,7 +22,7 @@ import javax.swing.JFrame;
  * o - open map file
  * enter - print course
  * esc - quit
- * 
+ *
  *
  * @author Arden Zhang
  * @author Andrew Wollack
@@ -33,13 +33,15 @@ public class SuperGUI {
 
 	public static final double FIELD_LENGTH = 54; // feet
 	public static final double FIELD_WIDTH = 27; // feet
-	public static final double CORNER_WIDTH = (FIELD_WIDTH-22)/2; // feet
+	public static final double CORNER_LENGTH = 2 + 11d/12; // length of corners (x) in feet
+	public static final double CORNER_WIDTH = (FIELD_WIDTH-22)/2; // width of corners (y) in feet
+	public static final double FENCE_WIDTH = 0.1; // width of the field borders in feet. Temporarily set to 0.1
 	public static final int SCALE = 40; // px/ft
 
 	public static final double ROBOT_LENGTH = 33d / 12; // feet
 	public static final double ROBOT_WIDTH = 28d / 12; // feet
 	public static final double ROBOT_DIAMETER = Math.sqrt(Math.pow(ROBOT_LENGTH, 2) + Math.pow(ROBOT_WIDTH, 2));
-	
+
 	public static final boolean WRITE_COMMAND = false; // write the code to a command and generate autoChooser
 	public static final boolean WRITE_MAP = true; // write the code to a readable map file
 	public static final String COMMANDS_DIRECTORY = "src/org/usfirst/frc/team2537/autocommands/";
@@ -47,17 +49,17 @@ public class SuperGUI {
 
 	public static final String FIELD_IMAGE = "src/FIELD.png";
 	public static final String AUTOCHOOSER_LOCATION = "src/maps/AutoChooser.java"; // src/org/usfirst/frc/team2537/robot/auto/AutoChooser.java
-	
+
 	public static final Color cursorColor = new Color(255, 0, 0);
 	public static final Color obstacleColor = new Color(0, 0, 0, 200);
-	
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("SuperGUI");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-//		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
-//		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
-//		frame.getContentPane().setCursor(blankCursor);
+		//		BufferedImage cursorImg = new BufferedImage(16, 16, BufferedImage.TYPE_INT_ARGB);
+		//		Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
+		//		frame.getContentPane().setCursor(blankCursor);
 
 		SuperPanel panel = new SuperPanel();
 
--- SuperGUI/src/SuperObstacle.java
+++ SuperGUI/src/SuperObstacle.java
@@ -1,21 +1,36 @@
 import java.awt.Polygon;
-import java.awt.Rectangle;
 
 public enum SuperObstacle {
 	RED_SWITCH(14 - 7d/3, 7 + 5d/48, 14d/3, 12 + 9.5/12),
 	SCALE(25, 8, 4, 11),
-	BLUE_SWITCH(113d/3, 7 + 5d/48, 14d/3, 12 + 9.5/12);
-	
+	BLUE_SWITCH(113d/3, 7 + 5d/48, 14d/3, 12 + 9.5/12),
+	TOP_LEFT_CORNER(0, SuperGUI.CORNER_WIDTH, SuperGUI.CORNER_LENGTH, 0, SuperGUI.FENCE_WIDTH),
+	TOP_RIGHT_CORNER(SuperGUI.FIELD_LENGTH - SuperGUI.CORNER_LENGTH, 0, SuperGUI.FIELD_LENGTH, SuperGUI.CORNER_WIDTH, SuperGUI.FENCE_WIDTH),
+	BOTTOM_LEFT_CORNER(0, SuperGUI.FIELD_WIDTH - SuperGUI.CORNER_WIDTH, SuperGUI.CORNER_LENGTH, SuperGUI.FIELD_WIDTH, SuperGUI.FENCE_WIDTH),
+	BOTTOM_RIGHT_CORNER(SuperGUI.FIELD_LENGTH - SuperGUI.CORNER_LENGTH, SuperGUI.FIELD_WIDTH, SuperGUI.FIELD_LENGTH, SuperGUI.FIELD_WIDTH - SuperGUI.CORNER_WIDTH, SuperGUI.FENCE_WIDTH);
+
 	final Polygon shape;
 	private SuperObstacle(double x, double y, double width, double height) {
-		Rectangle tmp = new Rectangle((int) (x*SuperGUI.SCALE), (int) (y*SuperGUI.SCALE), (int) (width*SuperGUI.SCALE), (int) (height*SuperGUI.SCALE));
-		int minX = (int) tmp.getMinX();
-		int maxX = (int) tmp.getMaxX();
-		int minY = (int) tmp.getMinY();
-		int maxY = (int) tmp.getMaxY();
-		shape = new Polygon(new int[] {minX, maxX, maxX, minX}, new int[] {minY, minY, maxY, maxY}, 4);
+		this(new double[] {x, x+width, x+width, x}, new double[] {y, y, y+height, y+height});
 	}
-	
+
+	private SuperObstacle(double x1, double y1, double x2, double y2, double width) {
+		double angle = Math.atan2(-y2 + y1, x2 - x1);
+		shape = new Polygon(new int[] {
+				(int) (x1*SuperGUI.SCALE + .5 * width * SuperGUI.SCALE * Math.cos(angle + Math.PI / 2)),
+				(int) (x1*SuperGUI.SCALE + .5 * width * SuperGUI.SCALE * Math.cos(angle - Math.PI / 2)),
+				(int) (x2*SuperGUI.SCALE + .5 * width * SuperGUI.SCALE * Math.cos(angle - Math.PI / 2)),
+				(int) (x2*SuperGUI.SCALE + .5 * width * SuperGUI.SCALE * Math.cos(angle + Math.PI / 2)) },
+
+				new int[] {
+						(int) (y1*SuperGUI.SCALE - .5 * width * SuperGUI.SCALE * Math.sin(angle + Math.PI / 2)),
+						(int) (y1*SuperGUI.SCALE - .5 * width * SuperGUI.SCALE * Math.sin(angle - Math.PI / 2)),
+						(int) (y2*SuperGUI.SCALE - .5 * width * SuperGUI.SCALE * Math.sin(angle - Math.PI / 2)),
+						(int) (y2*SuperGUI.SCALE - .5 * width * SuperGUI.SCALE * Math.sin(angle + Math.PI / 2)) },
+				4);
+
+	}
+
 	private SuperObstacle(double[] xpoints, double[] ypoints) {
 		if(xpoints.length != ypoints.length) throw new IllegalArgumentException();
 		int[] scaledXpoints = new int[xpoints.length];
